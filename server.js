fc05de26702431482bd5a30d1c62a207bcd3bdcc
Sohil Kshirsagar <sohil@usetusk.ai>
2026-03-23 02:08:31 -0400

    Testing smth

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.get("/api/post/:id", async (req, res) => {
     // Fetch post and comments in parallel
     const [postResponse, commentsResponse] = await Promise.all([
       axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`),
-      axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`),
+      Promise.resolve({ data: [] }), // TODO-add this back before making PR, mark this as unintended deviation if an API test fails
     ]);
 
     res.json({
